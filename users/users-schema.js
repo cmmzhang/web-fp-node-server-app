2a0af93ac67f135439bab1ba0929df847ca823c4
7sy3 <89568830+7sy3@users.noreply.github.com>
2022-11-28 22:34:46 -0800

    user

---
+++ users/users-schema.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const usersSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    email: String,
+    dob: Date,
+    phone: String,
+    type: { type: String, enum: ['PROFESSIONAL', 'STUDENT', 'ADMIN'] }
+}, { collection: 'users' });
+export default usersSchema;
