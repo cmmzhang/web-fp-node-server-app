5c4b569c79fbea23e087dde4b3d334758d1fd326
qmjin <jin.qimi@northeastern.edu>
2022-12-02 10:21:50 -0800

    likes

--- likes/likes-schema.js
+++ likes/likes-schema.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 const likesSchema = mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
-  book: {type: mongoose.Schema.Types.ObjectId, ref: 'BookModel'},
+  book: {type: mongoose.Schema.Types.String, ref: 'BookModel'},
 }, {collection: 'likes'})
 export default likesSchema
