5f47f17a9d46589988c2408adafd376bc08b1655
qmjin <jin.qimi@northeastern.edu>
2022-12-02 21:40:41 -0800

    latest version

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,7 +1,6 @@
 import * as dao from './users-dao.js'
 import { findByUsername } from './users-dao.js'
 
-
 let currentUser = null
 const UsersController = (app) => {
 
@@ -31,38 +30,38 @@ const UsersController = (app) => {
 
     const login = async (req, res) => {
         const credentials = req.body
-        const existingUser = await dao.findByCredentials(
-            credentials.username, credentials.password)
-        if(existingUser) {
-            req.session['currentUser'] = existingUser
-            res.json(existingUser)
+        const existingUser = await findByUsername(credentials.username)
+        if (!existingUser) {
+            res.sendStatus(403)
+            return
+        }
+        currentUser = existingUser
+        res.json(existingUser)
+    }
+
+    const register = async (req, res) => {
+        const user = req.body
+        const existingUser = await findByUsername(user.username)
+        if (existingUser) {
+            res.sendStatus(403)
+            return
+        }
+        const actualUser = await dao.createUser(user)
+        currentUser = actualUser
+        res.json(actualUser)
+    }
+
+
+    const profile = async (req, res) => {
+        if (currentUser) {
+            res.json(currentUser)
             return
         }
         res.sendStatus(403)
     }
 
-    const register = async (req, res) => {
-        const user = req.body;
-        const existingUser = await dao.findByUsername(user.username)
-        if(existingUser) {
-            res.sendStatus(403)
-            return
-        }
-        const currentUser = await dao.createUser(user)
-        req.session['currentUser'] = currentUser
-        res.json(currentUser)
-    }
-
-    const profile = async (req, res) => {
-        if (req.session['currentUser']) {
-            res.send(req.session['currentUser'])
-        } else {
-            res.sendStatus(403)
-        }
-    }
-
-    const logout = (req, res) => {
-        req.session.destroy()
+    const logout = async (req, res) => {
+        currentUser = null
         res.sendStatus(200)
     }
 
