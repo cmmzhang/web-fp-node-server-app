9e98e1cc20857e9a27f95fda1bc9a573efb1c2b6
qmjin <jin.qimi@northeastern.edu>
2022-12-04 17:34:18 -0800

    like sync with user

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session'
 import BooksController from "./books/books-controller.js";
 import LikesController from "./likes/likes-controller.js";
 import UsersController from "./users/users-controller.js";
+import FollowsController from "./follows/follows-controller.js";
+import SessionController from "./session-controller.js";
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,9 +21,24 @@ const options = {
 mongoose.connect('mongodb://localhost:27017/bookworms', options);
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors(
+    {
+      credentials: true,
+      origin: 'http://localhost:3000'
+    }
+))
+
+app.use(session({
+  secret: 'should be an environment variable',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use(express.json({}));
+
 BooksController(app);
 LikesController(app);
 UsersController(app);
+SessionController(app);
+FollowsController(app);
 app.listen(4000)
--- users/users-controller.js
+++ users/users-controller.js
@@ -30,38 +30,39 @@ const UsersController = (app) => {
 
     const login = async (req, res) => {
         const credentials = req.body
-        const existingUser = await findByUsername(credentials.username)
-        if (!existingUser) {
-            res.sendStatus(403)
-            return
-        }
-        currentUser = existingUser
-        res.json(existingUser)
-    }
-
-    const register = async (req, res) => {
-        const user = req.body
-        const existingUser = await findByUsername(user.username)
-        if (existingUser) {
-            res.sendStatus(403)
-            return
-        }
-        const actualUser = await dao.createUser(user)
-        currentUser = actualUser
-        res.json(actualUser)
-    }
-
-
-    const profile = async (req, res) => {
-        if (currentUser) {
-            res.json(currentUser)
+        const existingUser = await dao.findByCredentials(
+            credentials.username, credentials.password)
+        if(existingUser) {
+            req.session['currentUser'] = existingUser
+            res.json(existingUser)
             return
         }
         res.sendStatus(403)
     }
 
-    const logout = async (req, res) => {
-        currentUser = null
+    const register = async (req, res) => {
+        const user = req.body;
+        const existingUser = await dao.findByUsername(user.username)
+        if(existingUser) {
+            res.sendStatus(403)
+            return
+        }
+        const currentUser = await dao.createUser(user)
+        req.session['currentUser'] = currentUser
+        res.json(currentUser)
+    }
+
+
+    const profile = async (req, res) => {
+        if (req.session['currentUser']) {
+            res.send(req.session['currentUser'])
+        } else {
+            res.sendStatus(403)
+        }
+    }
+
+    const logout = (req, res) => {
+        req.session.destroy()
         res.sendStatus(200)
     }
 
