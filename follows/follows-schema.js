dbe21fb05a0b8cc1f4fc78dd69b18c9690a1a985
qmjin <jin.qimi@northeastern.edu>
2022-12-04 19:02:00 -0800

    likes

--- follows/follows-schema.js
+++ follows/follows-schema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const followsSchema = mongoose.Schema({
-  followed: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
-  follower: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
+  followed: {type: mongoose.Schema.Types.ObjectId, ref: 'usersModel'},
+  follower: {type: mongoose.Schema.Types.ObjectId, ref: 'usersModel'},
 }, {collection: 'follows'})
 
 export default followsSchema
