0182806503a1a1883e00a186a88ee54003abf9fd
“YangyiLi001” <yangyili1994@gmail.com>
2022-12-10 18:24:33 -0800

    update delete review

--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -3,6 +3,8 @@ import reviewsModel from "./reviews-model.js";
 export const createReview = async(review) =>{
     console.log("create review in reviews-dao",review)
     const actualReview = await reviewsModel.create(review)
+    console.log("actual review in reviews-dao", actualReview)
+    actualReview.author = review.author
     return actualReview
 
   }
@@ -11,7 +13,7 @@ export const createReview = async(review) =>{
   await reviewsModel.find()
 
 export const findReviewsByBook = async(booksapiID) =>{    
-    console.log("findReviewsByBook1 in reviews-dao",booksapiID)
+    // console.log("findReviewsByBook1 in reviews-dao",booksapiID)
     return await reviewsModel.find({booksapiID})
     .populate('author')
     .exec()
@@ -21,3 +23,7 @@ export const findReviewsByBook = async(booksapiID) =>{
 
 export const findReviewsByAuthor = (author) =>
     reviewsModel.find({author}) 
+
+export const deleteReview = async(review_id, author)=>{
+  return await reviewsModel.deleteOne({_id: review_id, author: author})
+}
